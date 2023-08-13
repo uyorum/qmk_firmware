fa36f7a6e9a018a72abcd0d075f35c9ed23ec355
uyorum <uyorum.pub@gmail.com>
2023-08-14 00:55:28 +0900

    update

--- keyboards/ergo42/keymaps/uyorum/config.h
+++ keyboards/ergo42/keymaps/uyorum/config.h
@@ -31,4 +31,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
--- keyboards/ergo42/keymaps/uyorum/keymap.c
+++ keyboards/ergo42/keymaps/uyorum/keymap.c
@@ -108,16 +108,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Meta3| LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |   _  |  UP  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * |TenKey| LGUI |AltTab| LAlt |   \  |CtlSpc| Meta1|   | Meta2|SftSpc|   -  |   ^  | LEFT | RIGHT| DOWN |
+   * |TenKey| LGUI |AltTab| LAlt |Ctrl/\| Space| Meta1|   | Meta2| Space|RSft/-|   ^  | LEFT | RIGHT| DOWN |
    * `------------------------------------------------'   `------------------------------------------------'
    */
 
 	[BASE] = LAYOUT( \
-		KC_ESC,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,              KC_Y,              KC_U,           KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
-		KC_GRV,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,          KC_G,              KC_H,              KC_J,           KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-		MO(META3),  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,              KC_N,              KC_M,           KC_COMM, KC_DOT, KC_SLSH, KC_RO,   KC_UP, \
-		TG(TENKEY), KC_LGUI, ALT_TAB, KC_LALT, KC_JYEN, CTL_T(KC_SPC), LT(META1, KC_F19), LT(META2, KC_F20), RSFT_T(KC_SPC), KC_MINS, KC_EQL, KC_LEFT, KC_RGHT, KC_DOWN \
-		),
+    KC_ESC,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,              KC_Y,              KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
+    KC_GRV,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,              KC_H,              KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
+    MO(META3),  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,              KC_N,              KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RO,   KC_UP, \
+    TG(TENKEY), KC_LGUI, ALT_TAB, KC_LALT, CTL_T(KC_JYEN), KC_SPC, LT(META1, KC_F19), LT(META2, KC_F20), KC_SPC, RSFT_T(KC_MINS), KC_EQL, KC_LEFT, KC_RGHT, KC_DOWN \
+    ),
 
   /* Meta 1
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -132,11 +132,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   
   [META1] = LAYOUT( \
-	  KC_NO, KC_F1,  KC_F2,   KC_F3,  KC_F4,  KC_F5,   LSFT(KC_8), LSFT(KC_9), KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, \
-		KC_NO, KC_F11, KC_F12,  KC_F13, KC_F14, KC_F15,  KC_RBRC,    KC_NUHS,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-		KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
-		KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_LCTL, KC_TRNS,    KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
-	  ),
+    KC_NO, KC_F1,  KC_F2,   KC_F3,  KC_F4,  KC_F5,   LSFT(KC_8), LSFT(KC_9), KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, \
+    KC_NO, KC_F11, KC_F12,  KC_F13, KC_F14, KC_F15,  KC_RBRC,    KC_NUHS,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+    KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO,   KC_NO,      KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, \
+    KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_LCTL, KC_TRNS,    KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO \
+    ),
 
   /* Meta 2
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -151,11 +151,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 	[META2] = LAYOUT( \
-	  KC_NO, KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       LSFT(KC_8),    LSFT(KC_9),    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,  LALT(KC_PSCR), \
-		KC_NO, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), KC_RBRC,       KC_NUHS,       LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), KC_NO, KC_PSCR, \
-		KC_NO, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      LSFT(KC_RBRC), LSFT(KC_NUHS), KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO, KC_NO, \
-		KC_NO, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LCTL,    KC_NO,         KC_TRNS,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO, KC_NO \
-		),
+    KC_NO, KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       LSFT(KC_8),    LSFT(KC_9),    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,  LALT(KC_PSCR), \
+    KC_NO, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), KC_RBRC,       KC_NUHS,       LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), KC_NO, KC_PSCR, \
+    KC_NO, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      LSFT(KC_RBRC), LSFT(KC_NUHS), KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO, KC_NO, \
+    KC_NO, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LCTL,    KC_NO,         KC_TRNS,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO, KC_NO \
+    ),
 
   /* Meta 3
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -170,11 +170,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 	[META3] = LAYOUT( \
-	  TO(0),   KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, PASS1, PASS5, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLEP, \
-		KC_NO,   KC_VOLD, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS2, PASS6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-		KC_TRNS, KC_MUTE, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS3, PASS7, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS4, PASS8, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
-		),
+    TO(0),   KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, PASS1, PASS5, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLEP, \
+    KC_NO,   KC_VOLD, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS2, PASS6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_TRNS, KC_MUTE, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS3, PASS7, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, PASS4, PASS8, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
+    ),
 
   /* Ten key
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -189,10 +189,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 	[TENKEY] = LAYOUT( \
-	  TO(0),   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8,    KC_P9,   KC_PMNS, KC_PSLS, KC_BSPC, \
-		KC_GRV,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5,    KC_P6,   KC_PPLS, KC_PAST, KC_ENT, \
-		KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2,    KC_P3,   KC_PCMM, KC_NO,   KC_NO, \
-		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, ZEROZERO, KC_PDOT, KC_PEQL, KC_NO,   KC_NO \
-		)
+    TO(0),   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8,    KC_P9,   KC_PMNS, KC_PSLS, KC_BSPC, \
+    KC_GRV,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5,    KC_P6,   KC_PPLS, KC_PAST, KC_ENT, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2,    KC_P3,   KC_PCMM, KC_NO,   KC_NO, \
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, ZEROZERO, KC_PDOT, KC_PEQL, KC_NO,   KC_NO \
+    )
 };
 
